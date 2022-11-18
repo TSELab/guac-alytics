075970c23673d8b7bcdfebe841efced1627f9547
SahithiKasim <71808684+SahithiKasim@users.noreply.github.com>
2022-11-17 20:58:51 -0500

    Update and rename scripts/ingestion/parsers/maintainer_db_init.py to scripts/ingestion/database/maintainer_db_init.py

--- scripts/ingestion/database/maintainer_db_init.py
+++ scripts/ingestion/database/maintainer_db_init.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 import sqlite3
 
-conn = sqlite3.connect('/data/yellow/vineet/database/bi_multi_tables.db')
-cur = conn.cursor()
+def target_con(con):
+        conn = sqlite3.connect(con)
+        cur = conn.cursor()
+        return cur
+
+target_con('/data/yellow/vineet/database/bi_multi_tables.db')
 cur.execute("""CREATE TABLE IF NOT EXISTS maintainer(
         name text primary key,
         inst integer,
