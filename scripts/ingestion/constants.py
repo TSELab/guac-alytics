0f45b68c22581d158816129c3f73ba975d397653
SahithiKasim <71808684+SahithiKasim@users.noreply.github.com>
2022-12-03 14:18:12 -0500

    Create constants.py

---
+++ scripts/ingestion/constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+db_loc = '/data/yellow/vineet/database/bi_multi_tables.db'
+maint_inst_loc = "https://popcon.debian.org/maint/by_inst"
+maintainer_tar_file = '/data/yellow/vineet/python_files/new_scripts/database_creation/maintainer.txt'
+regex = "(\ )+"
+inst_loc = 'https://popcon.debian.org/by_inst'
