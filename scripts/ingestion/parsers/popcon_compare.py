c14dbc4b8d178eac4424cbb9f4e3e0fb0ea9bf2a
SahithiKasim <71808684+SahithiKasim@users.noreply.github.com>
2022-12-09 10:03:41 -0500

    Update popcon_compare.py

--- scripts/ingestion/parsers/popcon_compare.py
+++ scripts/ingestion/parsers/popcon_compare.py
@@ -4,7 +4,7 @@ import os.path
 import re
 import csv
 from bs4 import BeautifulSoup as bs 
-from scripts.ingestion.constants import POPCON_CSV,POPCON_TEXT,INST_LOC,DB_LOC
+from scripts.ingestion.constants import POPCON_CSV,POPCON_TEXT,INST_LOC,DB_LOC,REGEX
 from scripts.ingestion.database.popcon_db_init import target_con
 
 x=datetime.now()
@@ -12,6 +12,7 @@ print('On ', datetime.strftime(x,"%m/%d/%Y"))
 
 #Connecting to database
 target_con(DB_LOC)
+
 def parse_line(line):
     # Header format
     # rank name inst vote old recent no-files (maintainer)
@@ -28,7 +29,7 @@ def parse_line(line):
         print(e)
 
     
-    comma_divided = re.sub(regex, ",", rest)
+    comma_divided = re.sub(REGEX, ",", rest)
     rank, name, inst, vote, old, recent, no_files, _ = \
             comma_divided.split(",")
     # print(maintainer)
@@ -78,46 +79,46 @@ def parser():
                 o.writerow(line)    
 
 
-        fdin.close()
-        fdout.close()
-        os.remove(POPCON_TEXT)   
+    fp.close()
+    fdout.close()
+    os.remove(POPCON_TEXT)   
 
-        year=date.today().year
-        mon=date.today().month
-        day=date.today().day
+    year=date.today().year
+    mon=date.today().month
+    day=date.today().day
 
-        # Saving data of each day
-        shutil.copyfile(POPCON_CSV,'/data/yellow/vineet/raw_data/popularity_contest/{}/{}/{}'.format(year,mon,day))
+    # Saving data of each day
+    shutil.copyfile(POPCON_CSV,'/data/yellow/vineet/raw_data/popularity_contest/{}/{}/{}'.format(year,mon,day))
 
-        # Comparing the data from csv file and existing table and updating its values 
-        with open(POPCON_CSV, 'r',encoding= 'unicode_escape') as file:
-                data = csv.reader(file,delimiter=',')   
-                no_records = 0 
-                nor=amd/l
-                for row in data:
-                    n=row[1]
-                    i=row[2]
-                    v=row[3]
-                    o=row[4]
-                    r=row[5]
-                    no=row[6]
-                    ia=(float(row[2])*nor)
-                    va=(float(row[3])*nor)
-                    maintainer=row[7]
-                    y=datetime.strftime(x,"%m/%d/%Y")
-                    # Updating the date if the number of votes of the package is changed
-                    cur.execute('''UPDATE popularity_table SET date=(?) WHERE name=(?) and vote!=(?)''',(y,n,v))  
-                    cur.execute(''' UPDATE popularity_table SET inst=(?), vote=(?), old=(?), recent=(?), no_files=(?), inst_norm=(?), vote_norm=(?) WHERE name=(?)''', (i,v,o,r,no,ia,va,n))          
-                    # Inserting new values into the table 
-                    cur.execute('''INSERT OR REPLACE INTO popularity_table VALUES (?,?,?,?,?,?,?,?,?,?)''',(n,y,i,v,o,r,no,maintainer,ia,va))
-                    conn.commit()
-                    no_records += 1
-                    if(no_records%25000==0):
-                        print(no_records, ' checked')
+    # Comparing the data from csv file and existing table and updating its values 
+    with open(POPCON_CSV, 'r',encoding= 'unicode_escape') as file:
+            data = csv.reader(file,delimiter=',')   
+            no_records = 0 
+            nor=amd/l
+            for row in data:
+                n=row[1]
+                i=row[2]
+                v=row[3]
+                o=row[4]
+                r=row[5]
+                no=row[6]
+                ia=(float(row[2])*nor)
+                va=(float(row[3])*nor)
+                maintainer=row[7]
+                y=datetime.strftime(x,"%m/%d/%Y")
+                # Updating the date if the number of votes of the package is changed
+                cur.execute('''UPDATE popularity_table SET date=(?) WHERE name=(?) and vote!=(?)''',(y,n,v))  
+                cur.execute(''' UPDATE popularity_table SET inst=(?), vote=(?), old=(?), recent=(?), no_files=(?), inst_norm=(?), vote_norm=(?) WHERE name=(?)''', (i,v,o,r,no,ia,va,n))          
+                # Inserting new values into the table 
+                cur.execute('''INSERT OR REPLACE INTO popularity_table VALUES (?,?,?,?,?,?,?,?,?,?)''',(n,y,i,v,o,r,no,maintainer,ia,va))
+                conn.commit()
+                no_records += 1
+                if(no_records%25000==0):
+                    print(no_records, ' checked')
 
-        print('Closing file')
-        file.close()
-        os.remove(POPCON_CSV)
-        conn.close()
+    print('Closing file')
+    file.close()
+    os.remove(POPCON_CSV)
+    conn.close()
         
 parser()
