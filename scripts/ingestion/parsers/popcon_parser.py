7cbaac2d75f6fff0e5479e331f320c722bb2ed23
SahithiKasim <71808684+SahithiKasim@users.noreply.github.com>
2022-12-09 00:45:45 -0500

    Update popcon_parser.py

--- scripts/ingestion/parsers/popcon_parser.py
+++ scripts/ingestion/parsers/popcon_parser.py
@@ -1,49 +1,55 @@
-import csv
-from curses import noraw
-from datetime import date, datetime
-from bs4 import BeautifulSoup as bs 
+#!/usr/bin/env python
 import requests
-import csv
 import os
-import shutil
+import re
+import csv
 from scripts.ingestion.constants import INST_LOC,POPCON_TEXT,POPCON_CSV
 
 x=datetime.now()
+        
+def parse_line(line):
+    # Header format
+    # rank name inst vote old recent no-files (maintainer)
+    rank, name, inst, vote, old, recent, no_files, maintainer = \
+        None, None, None, None, None, None, None, None
+
+    line = line.strip()
+    # print(line)
+    try:
+        rest, maintainer = line.split("(")
+        maintainer = maintainer.replace(")","") # Replace 
+    except Exception as e:
+        print(line)
+        print(e)
+
+    
+    comma_divided = re.sub(REGEX, ",", rest)
+    rank, name, inst, vote, old, recent, no_files, _ = \
+            comma_divided.split(",")
+    # print(maintainer)
+    return (rank, name, inst, vote, old, recent, no_files, maintainer)
 
 def parser():
-        print('On ', datetime.strftime(x,"%m/%d/%Y"))
-        # Getting data from popularity contest
-        page = requests.get(INST_LOC)
+    if os.path.exists(POPCON_TEXT):
+        print("using local copy of the file")
+    else:
+        print("downloading file")
+        response = requests.get(INST_LOC)
+        with open(POPCON_TEXT, "wt") as fp:
+            fp.write(response.text)
 
-        data = bs(page.content,"html.parser")
-        data = str(data)
+    with open(POPCON_TEXT, "rt") as fp:
+        data = fp.read()
 
-        # Splitting the data to store in a text file
-        if ('\n\n') in data:
-                data = data.split('\n')[1]
-        else:
-                data = data
-                
-        # Storing the popularity contest values in a text file
-        with open(POPCON_TEXT, 'w', encoding="utf-8") as fdin:
-                fdin.write(data)
-                print("Downloaded Data")
-        
-        fdin.close()
-
-        # Copying the data from text file to csv 
-        with open(POPCON_TEXT, 'r', encoding="utf-8") as fdin, open(POPCON_CSV,'w', encoding="utf-8",newline='') as fdout:
-                wr = csv.DictWriter(fdout, fieldnames=['sno','Name', 'inst', 'vote', 'old', 'recent', 'no_files', 'maintainer'], extrasaction='ignore')  # ignore unwanted fields 
-                o=csv.writer(fdout)
-                for line in fdin:
-                        # print("line is",line)
-                        # print(type(line))
-                        if not ('#' or '----' or 'Total' or '</') in line:
-                                o.writerow(line.split(None,7))
-
-        fdin.close()
-        fdout.close()
-        os.remove(POPCON_TEXT)
+    with open(POPCON_CSV,'w', encoding="utf-8",newline='') as fdout:
+            wr = csv.DictWriter(fdout, fieldnames=['sno','Name', 'inst', 'vote', 'old', 'recent', 'no_files', 'maintainer'], extrasaction='ignore')  # ignore unwanted fields 
+            o=csv.writer(fdout)
+            data = data.split("\n")
+            for line in data[:len(data)-3]:
+                if len(line) < 1 or line[0]=='#':
+                    continue
+                line = parse_line(line)
+                o.writerow(line)    
 
 
 
