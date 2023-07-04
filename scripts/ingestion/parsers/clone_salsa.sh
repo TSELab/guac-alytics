d28a9006126be867b3f2c03093a6defc33c57dd6
jialincheoh <jcheoh@purdue.edu>
2023-07-04 11:16:38 -0400

    include comment on group id

--- scripts/ingestion/parsers/clone_salsa.sh
+++ scripts/ingestion/parsers/clone_salsa.sh
@@ -4,5 +4,6 @@
 # This script git clone https://salsa.debian.org/python-team/packages
 # Create personal access token at https://salsa.debian.org/-/profile/personal_access_tokens
 # To clone with http url use http_url_to_repo
+# 9360 is the group-id of packages 
 
 for repo in $(curl -s --header "PRIVATE-TOKEN: my_token_here" https://salsa.debian.org/api/v4/groups/9360 | jq -r ".projects[].ssh_url_to_repo"); do git clone $repo; done;
