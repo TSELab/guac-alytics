502fb1be80c757ee57756451ad8571e69c3a723d
jialincheoh <jcheoh@purdue.edu>
2023-07-05 13:13:10 -0400

    combine cloning script and get repo info scripts

--- scripts/ingestion/parsers/clone_extract_salsa_info.sh
+++ scripts/ingestion/parsers/clone_extract_salsa_info.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#!/bin/bash 
 
-# Get all the repo information with attributes at https://docs.gitlab.com/ee/api/groups.html#list-groups
-
-# Output repo information to a file called projects.json 
-
-# Input is https://salsa.debian.org/python-team/packages
+# clone and get repo info from https://salsa.debian.org/python-team/packages
+# 9360 is the group id for packages 
+# project attributes can be found here https://docs.gitlab.com/ee/api/groups.html#list-a-groups-projects
 
 curl -H "PRIVATE-TOKEN: $GITLAB_TOKEN" "https://salsa.debian.org/api/v4/groups/9360/projects?per_page=10000" | jq 'map({ id: .id, name: .name, ssh_url: .ssh_url_to_repo, http_url: .http_url_to_repo, created_at: .created_at, statistics: .statistics,  star_count: .star_count, forks_count: .forks_count, last_activity_at: .last_activity_at, created_at: .created_at, path: .path, default_branch_protection: .default_branch_protection, open_issues_count: .open_issues_count, visibility: .visibility, archived: .archived, description: .description, topics: .topics, issues_enabled: .issues_enabled, merge_requests_enabled: .merge_requests_enabled, shared_with_groups: .shared_with_groups, request_access_enabled: .request_access_enabled })' > projects.json
+cat projects.json | jq ".[].http_url" -r | xargs -I {} git clone {}
--- scripts/ingestion/parsers/clone_salsa.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-# Register and request access from https://salsa.debian.org/public. Wait to be given access. 
-# This script git clone https://salsa.debian.org/python-team/packages
-# Create personal access token at https://salsa.debian.org/-/profile/personal_access_tokens
-# To clone with http url use http_url_to_repo
-# 9360 is the group-id of packages 
-
-for repo in $(curl -s --header "PRIVATE-TOKEN: my_token_here" https://salsa.debian.org/api/v4/groups/9360 | jq -r ".projects[].ssh_url_to_repo"); do git clone $repo; done;
