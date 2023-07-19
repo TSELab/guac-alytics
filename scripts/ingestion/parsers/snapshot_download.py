287998ba77840eb4d8c765c75a6087242528286f
absol27 <abhishekr272000@gmail.com>
2023-07-19 12:28:08 -0400

    Fix typo

--- scripts/ingestion/parsers/snapshot_download.py
+++ scripts/ingestion/parsers/snapshot_download.py
@@ -5,14 +5,14 @@ from os.path import exists
 
 def get_snapshot_bydate(date, ARCH):
     for DFSG in ["main", "contrib", "non-free"]:
-        if exists(f"./ingestion/publish_parsers/Packagelist_DUMP/{date}-{ARCH}-{DFSG}_Packages"):
+        if exists(f"./ingestion/parsers/Packagelist_DUMP/{date}-{ARCH}-{DFSG}_Packages"):
             continue # don't re-download
         while True:
             url = f"http://snapshot.debian.org/archive/debian/{date}/dists/stable/{DFSG}/binary-{ARCH}/Packages.gz"
             resp = requests.get(url)
             if resp.status_code == 200:
                 decompressed_file = gzip.decompress(resp.content)
-                with open(f"./ingestion/publish_parsers/Packagelist_DUMP/{date}-{ARCH}-{DFSG}_Packages", "w") as f:
+                with open(f"./ingestion/parsers/Packagelist_DUMP/{date}-{ARCH}-{DFSG}_Packages", "w") as f:
                     f.write(decompressed_file.decode('utf-8'))
                 break
             else:
