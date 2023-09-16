9649df2fc370d3782cef9be3f161080395db07b2
SahithiKasim <71808684+SahithiKasim@users.noreply.github.com>
2023-09-16 07:55:35 -0400

    .

--- scripts/ingestion/constants.py
+++ scripts/ingestion/constants.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-DB_LOC = '/data/yellow/vineet/database/bi_multi_tables.db'
+DB_LOC = './bi_multi_tables.db'
 MAINTAINER_INST_LOC = "https://popcon.debian.org/maint/by_inst"
 MAINTAINER_TEXT_FILE = '/data/yellow/vineet/python_files/new_scripts/database_creation/maintainer.txt'
 MAINTAINER_CSV_FILE = '/data/yellow/vineet/python_files/new_scripts/database_creation/maintainer.csv'
@@ -11,4 +11,3 @@ POPCON_CSV = '/data/yellow/vineet/python_files/new_scripts/data_pre_processing/t
 POPCON_TEXT = '/data/yellow/vineet/python_files/new_scripts/data_pre_processing/today.txt'
 VULNERABILITY_URL = 'https://security-tracker.debian.org/tracker/data/json'
 NIST_URL = 'https://nvd.nist.gov/vuln/detail/'
-VULNERABILITY_DATA = '/data/cyan/guacalytics/raw_data/vulnerability_data/{}/{}/{}'
